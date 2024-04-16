6bd584c5a871dab881c3b9652b4d206054e35298
optomaStanley <stanley.lin@optoma.com>
2024-04-16 14:25:30 +0800

    edit commands

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # tg-bot-eternal_ember
+
+##command list 
+
+- /officeal_document 官方文件
+- /tips_level_upgrade 升級攻略
+- /tips_equipment 裝備攻略
--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 import os
+import json
 
 TOKEN = os.environ.get('TOKEN')
+with open('tips.json') as f:
+    data = json.load(f)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # 回复使用者 /start 命令
-    print('start')
-    await update.message.reply_text('Hello! bot test.')
+async def official_document(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # 回复使用者 /official_document 命令
+    print('official_document')
+    await update.message.reply_text(data['official_document'])
 
-async def test(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # 回复使用者 /start 命令
-    print('test')
-    await update.message.reply_text('Hello! test.')
+async def level_upgrade_tips(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # 回复使用者 /level_upgrade_tips 命令
+    print('level_upgrade_tips')
+    await update.message.reply_text(data['level_upgrade_tips'])
+
+async def equipment_tips(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # 回复使用者 /equipment_tips 命令
+    print('equipment_tips')
+    await update.message.reply_text(data['equipment_tips'])
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    # 回复使用者 /start 命令
+    # 回复使用者 /echo 命令
     print('echo')
     text = update.message.text
     await update.message.reply_text(f'You said {text}.')
@@ -25,10 +33,9 @@ def main():
     application = Application.builder().token(TOKEN).build()
 
     # 添加 /start 命令處理器
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-
-    application.add_handler(CommandHandler('test', test))
+    application.add_handler(CommandHandler('official_document', official_document))
+    application.add_handler(CommandHandler('level_upgrade_tips', level_upgrade_tips))
+    application.add_handler(CommandHandler('equipment_tips', equipment_tips))
     
     echo_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
     application.add_handler(echo_handler)
